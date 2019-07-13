487e7e3bfeded847fc36938e004db7c70a7dd950
tavira <quramolt@gmail.com>
2019-07-13 21:42:24 +0300

    Fix: open post description with wrong id

--- src/views/postDescription.js
+++ src/views/postDescription.js
@@ -3,7 +3,7 @@ import store from '../store';
 export default () => {
   const modal = document.getElementById('postDesc');
   const activeFeed = store.feeds[store.activeData.feedId - 1];
-  const { title, desc } = activeFeed.articles[activeFeed.articles.length - store.activeData.postId];
+  const { title, desc } = activeFeed.articles[store.activeData.postId - 1];
   const modalTitle = modal.querySelector('.modal-title');
   const modalContent = modal.querySelector('.modal-body');
   modalTitle.textContent = title;
